0ad710f2ff8131f7b72be88b8cce8b68ae0bf10a
Peter Antypas <peter.antypas@gmail.com>
2023-01-16 11:54:18 -0800

    WIP

--- latest/Firmware/WiFiAdapter/core/http_sys_api.c
+++ latest/Firmware/WiFiAdapter/core/http_sys_api.c
@@ -11,10 +11,13 @@
 #include "tiny-json.h"
 #include "http_utils.h"
 #include "types.h"
+#include "../bsp/bsp.h"
+#include "esp_ota_ops.h"
 
 static const char *TAG = "sys";
 
 static char __outbuff[128];
+static char __inbuff[1024];
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Handlers
@@ -45,6 +48,93 @@ esp_err_t http_sys_get_handler(httpd_req_t *req)
   return ESP_OK;
 }
 
+/**
+ * @brief This came from https://gitlab.com/kevinwolfe/esp32_template/-/blob/main/main/main.c
+ * 
+ * @param req 
+ * @return esp_err_t 
+ */
+static esp_err_t http_sys_ota_handler( httpd_req_t *req )
+{
+  httpd_resp_set_status( req, HTTPD_500 );    // Assume failure
+  
+  int ret, remaining = req->content_len;
+  //printf( "Receiving\n" );
+  
+  esp_ota_handle_t update_handle = 0 ;
+  const esp_partition_t *update_partition = esp_ota_get_next_update_partition(NULL);
+  //const esp_partition_t *running          = esp_ota_get_running_partition();
+  
+  if ( update_partition == NULL )
+  {
+    //printf( "Uh oh, bad things\n" );
+    goto return_failure;
+  }
+
+  //printf( "Writing partition: type %d, subtype %d, offset 0x%08x\n", update_partition-> type, update_partition->subtype, update_partition->address);
+  //printf( "Running partition: type %d, subtype %d, offset 0x%08x\n", running->type,           running->subtype,          running->address);
+  esp_err_t err = ESP_OK;
+  err = esp_ota_begin(update_partition, OTA_WITH_SEQUENTIAL_WRITES, &update_handle);
+  if (err != ESP_OK)
+  {
+    //printf( "esp_ota_begin failed (%s)", esp_err_to_name(err));
+    goto return_failure;
+  }
+
+  while ( remaining > 0 )
+  {
+    // Read the data for the request
+    if ( ( ret = httpd_req_recv( req, __inbuff, MIN( remaining, sizeof( __inbuff ) ) ) ) <= 0 )
+    {
+      if ( ret == HTTPD_SOCK_ERR_TIMEOUT )
+      {
+        // Retry receiving if timeout occurred
+        continue;
+      }
+
+      goto return_failure;
+    }
+    
+    size_t bytes_read = ret;
+    
+    remaining -= bytes_read;
+    err = esp_ota_write(update_handle, __inbuff, bytes_read);
+    if (err != ESP_OK)
+    {
+      goto return_failure;
+    }
+  }
+
+  //printf( "Receiving done\n" );
+
+  // End response
+  if ( esp_ota_end(update_handle) == ESP_OK && esp_ota_set_boot_partition(update_partition) == ESP_OK )
+  {
+    //printf( "OTA Success?!\n Rebooting\n" );
+    //fflush( stdout );
+
+    httpd_resp_set_status( req, HTTPD_200 );
+    httpd_resp_send( req, NULL, 0 );
+    
+    vTaskDelay( 2000 / portTICK_RATE_MS);
+    bsp_reboot();    
+    return ESP_OK;
+  }
+  //printf( "OTA End failed (%s)!\n", esp_err_to_name(err));
+
+return_failure:
+  if ( update_handle )
+  {
+    esp_ota_abort(update_handle);
+  }
+
+  httpd_resp_set_status( req, HTTPD_500 );    // Assume failure
+  httpd_resp_send( req, NULL, 0 );
+  return ESP_FAIL;
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////
 // URIs
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -56,10 +146,19 @@ httpd_uri_t uri_sys_get =
   .user_ctx = NULL
 };
 
+httpd_uri_t uri_sys_ota = 
+{
+  .uri = "/api/ota",
+  .method = HTTP_POST,
+  .handler = http_sys_ota_handler,
+  .user_ctx = NULL
+};
+
 ////////////////////////////////////////////////////////////////////////////////////////
 // Registration
 ////////////////////////////////////////////////////////////////////////////////////////
 void register_sys_api_handlers(httpd_handle_t handle)
 {
   httpd_register_uri_handler(handle, &uri_sys_get);
+  httpd_register_uri_handler(handle, &uri_sys_ota);
 }
